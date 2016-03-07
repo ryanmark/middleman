6102f5bd053c357b8eef16accdda5fc679493c28
Ryan Mark <ryan@mrk.cc>
2016-03-07 14:42:20 -0500

    fix asset-hash handling of minified files, middleman/middleman#1563

--- middleman-core/lib/middleman-more/extensions/asset_hash.rb
+++ middleman-core/lib/middleman-more/extensions/asset_hash.rb
@@ -63,7 +63,7 @@ class Middleman::Extensions::AssetHash < ::Middleman::Extension
       @rack_app        = app
       @exts            = options[:exts]
       @ignore          = options[:ignore]
-      @exts_regex_text = @exts.map { |e| Regexp.escape(e) }.sort.reverse.join('|')
+      @exts_regex_text = Regexp.union(@exts.sort.reverse)
       @middleman_app   = options[:middleman_app]
     end
 
@@ -91,7 +91,7 @@ class Middleman::Extensions::AssetHash < ::Middleman::Extension
       dirpath = Pathname.new(File.dirname(path))
 
       # TODO: This regex will change some paths in plan HTML (not in a tag) - is that OK?
-      body.gsub(/([=\'\"\(,]\s*)([^\s\'\"\)]+(#{@exts_regex_text}))/) do |match|
+      body.gsub(/([=\'\"\(,]\s*)([^\s\'\"\)]{1,2000}#{@exts_regex_text})/) do |match|
         opening_character = $1
         asset_path = $2
 
